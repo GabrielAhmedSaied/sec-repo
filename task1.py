b24aae630053cfa1432f162081359106d7657316
GabrielAhmedSaied <gbriel.ahmed2020@gmail.com>
2022-08-16 02:45:31 +0200

    add the task1 doc and the solve in .py

---
+++ task1.py
@@ -0,0 +1,79 @@
+#1
+x=50 ;print(x>40)
+##################2
+y=10 ;print(type(y))
+#############3
+z=10.5 ;print(type(z))
+########4
+s='gabriel';print(type(s))
+###########
+#5
+#convert the string to int
+#######
+#6nono
+########7
+r = [1, 2, 3, 4];print(type(r))
+#########8
+r2 = (1, 2, 3, 4, 5);print(type(r2))
+###########
+#9
+#convert list tuple
+############
+#10 create adict for 3 value
+##########
+#11 yes
+###########
+#12 pathon is interpreted or compiled
+##########
+#13 what is differences between low level ,high level
+###########
+#14 =(equaland assignment operator) ==(false ,true and comparison operator)
+##########
+#15 != (not equal)
+##########
+#16 what is the operator precedence
+##########
+#17
+x=10
+##########18
+x=x+5 ; print(x)
+#########19
+x=x/5 ; print(x)
+#####20
+x=x*10 ;print(x)
+###########21
+print(11/3)
+######22
+print(11%4)
+#######23
+print(2**3)
+########24
+print(11/4)
+####25 yes
+####26
+print(10>15) #false
+######27
+if (10<15) :
+    print('x<15')
+########28
+#all
+##########29
+#all,and
+########30
+#or,any
+#######31
+#all
+#######32
+#if contains one condition and elif contains another condition be side if
+#33#elif is a condition but else mean prin the value if the condition is false
+#34 #yes
+#35#yes
+#36
+print('x<15') if 10<15 else print('happy')
+#37yes
+#38for git the value if condition not happened
+#34yes
+#35yes
+#36not
+#36yes
+
