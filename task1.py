d8adb79f2f49ae1c3c60205ef7ca6b8a173b12bf
GabrielAhmedSaied <gbriel.ahmed2020@gmail.com>
2022-08-29 23:13:12 +0200

    task1 is modified

--- task1.py
+++ task1.py
@@ -1,26 +1,29 @@
-#1
+#1Create a Boolean variable named x
 x=50 ;print(x>40)
-##################2
+#2Create an integer variable named y
 y=10 ;print(type(y))
-#############3
+#############3 Create a float variable named z
 z=10.5 ;print(type(z))
-########4
+########4Create a string variable names s
 s='gabriel';print(type(s))
-###########
-#5
-#convert the string to int
-#######
-#6nono
-########7
+############5Convert the int variable to float
+n=float(100)
+print(type(n))
+#6yes
+#no
+########7Create a list of numbers from 1 to 5
 r = [1, 2, 3, 4];print(type(r))
 #########8
 r2 = (1, 2, 3, 4, 5);print(type(r2))
-###########
-#9
-#convert list tuple
-############
-#10 create adict for 3 value
-##########
+###########9Convert the list to tuple
+r3=tuple(r);print(r3)
+#############10 create adict for 3 value
+dict={
+    'ahmed': 80,
+    'osam':50,
+    'gbriel':40
+}
+##########●11 Can we use semi colon ; with python
 #11 yes
 ###########
 #12 pathon is interpreted or compiled
@@ -32,6 +35,8 @@ r2 = (1, 2, 3, 4, 5);print(type(r2))
 #15 != (not equal)
 ##########
 #16 what is the operator precedence
+#+,-,*,**,/,//,%
+
 ##########
 #17
 x=10
@@ -49,7 +54,10 @@ print(11%4)
 print(2**3)
 ########24
 print(11/4)
+print('@'*20)
 ####25 yes
+print(11%3)#2     yes   int 
+print(11/3)#3.666 no    float
 ####26
 print(10>15) #false
 ######27
@@ -64,16 +72,19 @@ if (10<15) :
 #######31
 #all
 #######32
-#if contains one condition and elif contains another condition be side if
-#33#elif is a condition but else mean prin the value if the condition is false
+#if contains one condition and elif contains another condition be side if on the same variable
+#33#elif is a condition but else mean print the value if the condition is false
 #34 #yes
 #35#yes
 #36
 print('x<15') if 10<15 else print('happy')
-#37yes
+#37no
 #38for git the value if condition not happened
-#34yes
-#35yes
-#36not
-#36yes
+
+#39
+list= [2,4,6,8,10]
+print(4 in list)
+print(4 in list and 6 in list )
+print(3 in list or 6 in list )
+print(2 in list or 4 in list and 5 in list  )
 
